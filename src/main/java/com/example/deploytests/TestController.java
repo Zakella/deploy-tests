0db27119afb9c6a6a9fff0d7f34531b6641ea2ff
Slava Zapolschi <zapolski.slava@gmail.com>
2023-05-01 12:07:52 +0300

    test 4

--- src/main/java/com/example/deploytests/TestController.java
+++ src/main/java/com/example/deploytests/TestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 //ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIElD9UZJvR/+rm4ve732Cq+/DzvC9rWJjawQtsEh4KIS zakella@zapolski
 
+
 @RestController
 @RequestMapping
 public class TestController {
