d9a7a080c44ee20553447edaeeedfca832a316c5
Slava Zapolschi <zapolski.slava@gmail.com>
2023-05-01 11:52:56 +0300

    Initial commit

---
+++ src/main/java/com/example/deploytests/TestController.java
@@ -0,0 +1,2 @@
+package com.example.deploytests;public class TestController {
+}
---
+++ src/test/java/com/example/deploytests/DeployTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deploytests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
