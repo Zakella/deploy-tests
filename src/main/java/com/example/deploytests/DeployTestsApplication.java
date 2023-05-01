7a2b430e2d8acc65a34f37afafb4fc940e6fbb01
Slava Zapolschi <zapolski.slava@gmail.com>
2023-05-01 11:55:36 +0300

    test 1

--- src/main/java/com/example/deploytests/DeployTestsApplication.java
+++ src/main/java/com/example/deploytests/DeployTestsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DeployTestsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DeployTestsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DeployTestsApplication.class, args);
+    }
 
 }
--- src/main/java/com/example/deploytests/TestController.java
+++ src/main/java/com/example/deploytests/TestController.java
@@ -1,2 +1,16 @@
-package com.example.deploytests;public class TestController {
+package com.example.deploytests;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class TestController {
+
+    @GetMapping
+    public String sayHello(){
+        return "Hello!!";
+    }
 }
